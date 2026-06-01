7b278c5a98fab70f4fd5c6ff89f9c30e8f4831a3
roku253 <roku510@au.com>
2026-06-01 15:02:35 +0900

    up

--- js/city-top.js
+++ js/city-top.js
@@ -3,19 +3,11 @@
     var slides = document.querySelectorAll(".city-top-hero__slide");
     if (slides.length < 2) return;
     var i = 0;
-    var playBtn = document.getElementById("city-hero-play");
-    var timer = setInterval(next, 6000);
+    setInterval(next, 6000);
     function next() {
       slides[i].classList.remove("is-active");
       i = (i + 1) % slides.length;
       slides[i].classList.add("is-active");
-    }
-    if (playBtn) {
-      playBtn.addEventListener("click", function () {
-        clearInterval(timer);
-        next();
-        timer = setInterval(next, 6000);
-      });
     }
   }
 
